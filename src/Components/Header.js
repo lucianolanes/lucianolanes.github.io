9d807a219cee96526cc2462e6148f10dab96b50f
Luciano Lanes <llaneslopes@gmail.com>
2021-09-17 23:55:57 -0300

    Atualizado CSS responsivo do componente de projetos para todas as resoluçoes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,6 +9,7 @@ export default function Header() {
       <Fade duration={3000} >
         <div className="header-container">
           <h1>Olá, sou o Luciano e este é meu Portfólio</h1>
+          <p>Página mobile em construção. Por favor, acesse por outro dispositivo</p>
         </div>
         <a href="#main-page"><img src={ pointer } alt="Apontando para baixo" /> <p>Iniciar</p></a>
       </Fade>
