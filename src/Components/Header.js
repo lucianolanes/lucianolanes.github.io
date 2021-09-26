807ccad61b4b1fc6c7dda15f6bc493754dd0ad30
Luciano Lanes <llaneslopes@gmail.com>
2021-09-26 18:42:36 -0300

    Aviso de construçao para resoluçoes mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,9 +7,9 @@ export default function Header() {
   return (
     <header>
       <Fade duration={3000} >
+        <h2 id="mobile">Página mobile em construção</h2>
         <div className="header-container">
           <h1>Olá, sou o Luciano e este é meu Portfólio</h1>
-          <p id="mobile">Página mobile em construção. Por favor, acesse por outro dispositivo</p>
         </div>
         <a href="#main-page"><img src={ pointer } alt="Apontando para baixo" /> <p>Iniciar</p></a>
       </Fade>
