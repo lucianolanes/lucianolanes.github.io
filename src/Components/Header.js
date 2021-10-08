7650541a0b8bb2dfa4a7d6a2e1de3238d73c08cc
Luciano Lanes <llaneslopes@gmail.com>
2021-10-08 00:14:34 -0300

    Adicionado efeito e som ao clicar no botao iniciar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
-import pointer from '../images/pointer.png'
+import pointer from '../images/pointer.png';
 import './Header.css';
+import pointerSound from '../soundEffects/cursor_sound.mp3';
 
 export default function Header() {
+  const playSound = () => {
+    const soundEffect = new Audio(pointerSound);
+    soundEffect.play();
+  }
+
   return (
     <header>
       <Fade duration={3000} >
@@ -11,7 +17,10 @@ export default function Header() {
         <div className="header-container">
           <h1>Olá, sou o Luciano e este é meu Portfólio</h1>
         </div>
-        <a href="#main-page"><img src={ pointer } alt="Apontando para baixo" /> <p>Iniciar</p></a>
+        <a href="#main-page">
+          <img src={ pointer } alt="Apontando para Esquerda" id="header-pointer" />
+          <p onClick={ playSound } className="iniciar">Iniciar</p>
+        </a>
       </Fade>
     </header>
   )
