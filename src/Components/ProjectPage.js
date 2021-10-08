91d4fd4a42a90cd6b88f0cf31d651417917dfb9b
Luciano Lanes <llaneslopes@gmail.com>
2021-10-08 11:53:31 -0300

    Adicionado efeito e pointer na project page

--- src/Components/ProjectPage.js
+++ src/Components/ProjectPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import PROJECTS from '../service/projects';
-import pointer from '../images/pointer.png'
+import pointer from '../images/pointer.png';
+import pointerSound from '../soundEffects/cursor_sound.mp3';
 import './ProjectPage.css';
 
 export default function FrontEndProj({id}) {
+  const playSound = () => {
+    const soundEffect = new Audio(pointerSound);
+    soundEffect.play();
+  }
+
   const renderProject = () => {
     const projeto = PROJECTS.filter((project) => project.id === id);
     return(
@@ -19,13 +25,17 @@ export default function FrontEndProj({id}) {
           </div>
         </div>
         <section className="project-links">
-        <div className="link-container">
-          <img src={ pointer } alt="Apontando para Direita" className="pointer"/>
-          <a href={projeto[0].pageLink} target="_blank" rel="noreferrer">Demonstração</a>
+        <div className="link-container" onClick={ playSound }>
+          <a href={projeto[0].pageLink} target="_blank" rel="noreferrer">
+            <img src={ pointer } alt="Apontando para Direita" className="project-pointer"/>
+            Demonstração
+          </a>
         </div>
-        <div className="link-container">
-          <img src={ pointer } alt="Apontando para Direita" className="pointer"/>
-          <a href={projeto[0].repoLink} target="_blank" rel="noreferrer">Repositório</a>
+        <div className="link-container" onClick={ playSound }>
+          <a href={projeto[0].repoLink} target="_blank" rel="noreferrer">
+            <img src={ pointer } alt="Apontando para Direita" className="project-pointer"/>
+            Repositório
+          </a>
         </div>
         </section>
       </Fade>
